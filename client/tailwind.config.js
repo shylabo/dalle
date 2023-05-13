5a13a21f10f4a80b44fc5fcd8f1381f9e5854613
shylabo <shuya.sohot@gmail.com>
2023-05-13 14:46:27 -0700

    Change Linter and config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/**/*.{js,jsx}"],
+  content: ['./src/**/*.{js,jsx}'],
   theme: {
     extend: {
       screens: {
-        xs: "480px",
+        xs: '480px',
       },
       fontFamily: {
-        inter: ["Inter var", "sans-serif"],
+        inter: ['Inter var', 'sans-serif'],
       },
       boxShadow: {
-        card: "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)",
-        cardhover:
-          "0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)",
+        card: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.2)',
+        cardhover: '0 0 1px 0 rgba(189,192,207,0.06),0 10px 16px -1px rgba(189,192,207,0.4)',
       },
     },
   },
